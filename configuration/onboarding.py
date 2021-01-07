d196e1dac913047dd29f07fcaffd40a9a419d575
Geoff Davis <geoff@geoffdavis.com>
2021-01-06 22:21:39 -0800

    Fix typo

--- configuration/onboarding.py
+++ configuration/onboarding.py
@@ -1,4 +1,4 @@
-rom netbox.configuration.configuration import PLUGINS, PLUGINS_CONFIG
+from netbox.configuration.configuration import PLUGINS, PLUGINS_CONFIG
 
 PLUGINS.append('netbox_onboarding')
 PLUGINS_CONFIG['netbox_onboarding'] = {
